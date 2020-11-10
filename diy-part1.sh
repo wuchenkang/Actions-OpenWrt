5795facc3bd78e8f7e2f99245519627398a96607
NanoDesu! <20625307+wuchenkang@users.noreply.github.com>
2020-11-11 00:11:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,24 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-package' feeds.conf.default
+
+# iputils
+svn co https://github.com/Lienol/openwrt/trunk/package/network/utils/iputils package/network/utils/iputils
+
+# New argon theme
+rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# Adguard Home
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/lean/luci-app-adguardhome
+
+# Smart DNS
+svn co  https://github.com/Lienol/openwrt-packages/trunk/net/smartdns package/lean/smartdns
+svn co  https://github.com/Lienol/openwrt-luci/trunk/applications/luci-app-smartdns package/lean/luci-app-smartdns
+
+# SYSU H3C
+svn co https://github.com/wuchenkang/openwrt-sysuh3c/trunk/luci-app-sysuh3c package/lean/luci-app-sysuh3c
+svn co https://github.com/wuchenkang/openwrt-sysuh3c/trunk/sysuh3c package/lean/sysuh3c
